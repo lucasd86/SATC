02f50a7cfb83160d1904534fa8f067c6fdb7e0ef
lucasd86 <lucas.m.davy@gmail.com>
2017-07-15 17:18:05 -0300

    proyecto

--- Lppa.UI.Website/Controllers/HomeController.cs
+++ Lppa.UI.Website/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace Lppa.UI.Website.Controllers
 
             return View();
         }
+
+        public ActionResult Solicitud()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
