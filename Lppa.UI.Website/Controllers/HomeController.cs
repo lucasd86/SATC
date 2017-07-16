a1e68a548e5f18bc2c3a8c89bef2c0af5301e1fd
Cristian Matias Martin <matiasmartin@live.com.ar>
2017-07-16 13:10:56 -0300

    Pantallas nuevas

--- Lppa.UI.Website/Controllers/HomeController.cs
+++ Lppa.UI.Website/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lppa.Entities;
 
 namespace Lppa.UI.Website.Controllers
 {
@@ -28,6 +29,30 @@ namespace Lppa.UI.Website.Controllers
         }
 
         public ActionResult Solicitud()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Solicitud(FormCollection Form)
+        {
+            ClienteTitular ClienteTitular = new ClienteTitular();
+
+            ClienteTitular.Apellido = Form.Get("apellidoTitular").ToString();
+            ClienteTitular.Nombre = Form.Get("nombreTitular").ToString();
+            ClienteTitular.FechaNacimiento = Convert.ToDateTime(Form.Get("fechaNacimiento").ToString());
+            ClienteTitular.DNI = Convert.ToInt32(Form.Get("numeroDocumento").ToString());
+            ClienteTitular.CUIT = Convert.ToInt32(Form.Get("cuilCuit").ToString());
+            ClienteTitular.Domicilio = Form.Get("domicilioTitular").ToString();
+            //ClienteTitular = Form.Get("numeroTelefono").ToString();
+            //ClienteTitular.DNIConyuge = Convert.ToInt32(Form.Get("numeroDocumentoCon").ToString()); 
+
+            RedirectToAction("DatosAdicionales", "Home");
+        }
+
+        public ActionResult DatosAdicionales(ClienteTitular Cliente)
         {
             ViewBag.Message = "Your contact page.";
 
