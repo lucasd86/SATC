3d87b9c56d30b73cb3ccb434075b89951b617562
lucasd86 <lucas.m.davy@gmail.com>
2017-07-17 22:13:29 -0300

    Agregado modelo de tarjetas

--- Lppa.UI.Website/Controllers/TarjetasController.cs
+++ Lppa.UI.Website/Controllers/TarjetasController.cs
@@ -17,10 +17,45 @@ namespace Lppa.UI.Website.Controllers
         // GET: Tarjetas
         public ActionResult Index()
         {
+
             var tarjetaDeCredito = db.TarjetaDeCredito.Include(t => t.ClienteTitular);
-            return View(tarjetaDeCredito.ToList());
+            List<Models.TarjetaModel> lista = new List<Models.TarjetaModel>();
+            foreach (var item in tarjetaDeCredito)
+            {
+                var tarjeta = new Models.TarjetaModel();
+                tarjeta.DNICliente = item.DNICliente;
+                tarjeta.NumeroTarjeta = item.NumeroTarjeta;
+                tarjeta.SaldoMaximo = item.SaldoMaximo;
+                tarjeta.Tipo = PasarTipo(item.Tipo) ;
+                tarjeta.Marca = PasarMarca(item.Marca);
+                tarjeta.Estado = PasarEstado(item.Estado);
+                lista.Add(tarjeta);
+            }
+
+            
+            return View(lista);
         }
 
-        
+        public string PasarMarca(int i)
+        {
+
+            return ((Lppa.Entities.MarcasTarjetasCredito)i).ToString();
+
+
+        }
+        public string PasarEstado(int i)
+        {
+
+            return ((Lppa.Entities.EstadoTarjeta)i).ToString();
+
+
+        }
+        public string PasarTipo(int i)
+        {
+
+            return ((Lppa.Entities.TipoDeTarjetaDeCredito)i).ToString();
+
+
+        }
     }
 }
---
+++ Lppa.UI.Website/Models/TarjetaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lppa.UI.Website.Models
+{
+    public class TarjetaModel
+    {
+
+        public int NumeroTarjeta { get; set; }
+        public string Marca { get; set; }
+        public int SaldoMaximo { get; set; }
+        public string Tipo { get; set; }
+        public int DNICliente { get; set; }
+        public string Estado { get; set; }
+
+
+    }
+}
